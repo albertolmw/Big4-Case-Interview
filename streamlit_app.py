74196d0d1865a907e6332c2a2c9f6324a6bb66fe
albertolmw <j.alberto28lm@outlook.com>
2025-01-07 21:45:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('First App')
 st.write('Hello World')
-path="Data.xlsx"
+path="https://github.com/albertolmw/DataScientist-Case-Interview/blob/main/Data.xlsx"
 df1 = pd.read_excel(path, sheet_name='Charges')
 df2 = pd.read_excel(path, sheet_name='Other data')
 df3 = pd.read_excel(path, sheet_name='Churn')
