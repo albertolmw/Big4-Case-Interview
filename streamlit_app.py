80bed6adf1d43a0958d68e71d412f0f16171bf81
albertolmw <j.alberto28lm@outlook.com>
2025-01-07 22:00:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('First App')
 st.write('Hello World')
 path='https://github.com/albertolmw/DataScientist-Case-Interview/blob/main/Data.xlsx'
 datos=rq.get(path)
-df1 = pd.read_excel(datos.content)
+df1 = pd.read_excel(datos.content,sheet_name='Other data')
 #df2 = pd.read_excel(path, sheet_name='Other data')
 #df3 = pd.read_excel(path, sheet_name='Churn')
 df1
