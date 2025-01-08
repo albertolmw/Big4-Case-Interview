380c9b845b62871a349a513e7217b1b48a4fef53
albertolmw <j.alberto28lm@outlook.com>
2025-01-07 21:47:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 st.title('First App')
 st.write('Hello World')
 path="https://github.com/albertolmw/DataScientist-Case-Interview/blob/main/Data.xlsx"
-df1 = pd.read_excel(path, sheet_name='Charges')
-df2 = pd.read_excel(path, sheet_name='Other data')
-df3 = pd.read_excel(path, sheet_name='Churn')
+df1=rq.get(path).content
+df1 = pd.read_excel(BytesIO(df1), sheet_name='Charges')
+#df2 = pd.read_excel(path, sheet_name='Other data')
+#df3 = pd.read_excel(path, sheet_name='Churn')
 df1
+
